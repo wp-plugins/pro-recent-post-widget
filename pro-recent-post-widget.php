aa0499e1725eae49e415db4d09a2a0b04e68ab5f
shambhu patnaik <shambhu patnaik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-04 08:02:06 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/pro-recent-post-widget/trunk@663060 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pro-recent-post-widget.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Plugin Name: Pro Recent Post Widget
+Plugin URI: http://aynsoft.com/
+Description: Pro Recent Post Widget plugin.You have choice to specific category recent post show
+Version: 1.0
+Author: Shambhu Prasad Patnaik
+Author URI:http://aynsoft.com/
+*/
+class Pro_Recent_Post_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+		parent::__construct(
+	 		'pro_recent_post', // Base ID
+			'Pro Recent Posts', // Name
+			array( 'description' => __( 'The most recent posts on your site', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+	 extract( $args );
+	 $title = apply_filters( 'widget_title', $instance['title'] );
+	 if ( empty( $instance['number'] ) || ! $number = absint( $instance['number'] ) )
+ 	 $number = 5;
+	 $show_date = isset( $instance['show_date'] ) ? $instance['show_date'] : false;
+	 $post_category = empty( $instance['post_category'] ) ? '' : $instance['post_category'];
+     $r = new WP_Query( apply_filters( 'widget_posts_args', array( 'posts_per_page' => $number, 'no_found_rows' => true, 'post_status' => 'publish', 'ignore_sticky_posts' => true,'cat'=>$post_category ) ) );
+	 //print_r($r);die();
+	 if ($r->have_posts()) :
+     ?>
+	 <?php echo $before_widget; ?>
+	 <?php if ( $title ) echo $before_title . $title . $after_title; ?>
+	 <ul>
+	 <?php while ( $r->have_posts() ) : $r->the_post(); ?>
+		<li>
+		<a href="<?php the_permalink() ?>" title="<?php echo esc_attr( get_the_title() ? get_the_title() : get_the_ID() ); ?>"><?php if ( get_the_title() ) the_title(); else the_ID(); ?></a>
+		<?php if ( $show_date ) : ?>
+		<span class="post-date"><?php echo get_the_date(); ?></span>
+		<?php endif; ?>
+		</li>
+	 <?php endwhile; ?>
+	 </ul>
+	 <?php echo $after_widget; ?>
+     <?php
+	 // Reset the global $the_post as this query will have stomped on it
+	 endif;
+	}
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['number'] = (int) $new_instance['number'];
+		$instance['show_date'] = (bool) $new_instance['show_date'];
+		$instance['post_category'] = strip_tags($new_instance['post_category']);
+		return $instance;
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+	 if ( isset( $instance[ 'title' ] ) ) {
+	  $title = $instance[ 'title' ];
+	 }
+	 else {
+	  $title = __( 'Recent Posts', 'text_domain' );
+ 	 }
+	 $number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+	 $show_date = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
+	 $post_category    = isset( $instance['post_category'] ) ? esc_attr($instance['post_category']) : '';
+	?>
+	 <p>
+	  <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+	   <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+	 </p>
+	 <p>
+	  <label for="<?php echo $this->get_field_id( 'post_category' ); ?>"><?php _e( 'Post Categories:' ); ?></label> 
+	  <input class="widefat" id="<?php echo $this->get_field_id( 'post_category' ); ?>" name="<?php echo $this->get_field_name( 'post_category' ); ?>" type="text" value="<?php echo esc_attr( $post_category); ?>" />
+	  <br />
+	  <small><?php _e( 'category IDs, separated by commas.' ); ?></small>
+	 </p>
+	 <p>
+	  <label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
+	  <input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $number; ?>" size="3" />
+	 </p>
+	 <p>
+	  <input class="checkbox" type="checkbox" <?php checked( $show_date ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" />
+	  <label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label>
+	 </p>
+	 <?php 
+	}
+} // class pro_recent_post
+
+// register Pro_Recent_Post_Widget widget
+add_action( 'widgets_init', create_function( '', 'register_widget( "Pro_Recent_Post_Widget" );' ) );
+register_deactivation_hook(__FILE__, 'shambhu_plugin_deactivate');
+
+function shambhu_plugin_deactivate ()
+{
+ unregister_widget('Pro_Recent_Post_Widget');
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Pro Recent Post Widget ===
+Contributors: shambhu patanik
+Donate link: http://aynsoft.com/
+Tags: pro recent post widget,widget,custom recent post,advanced recent posts widget	
+Requires at least: 3.0.1
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Here is a short description of the plugin.  This should be no more than 150 characters.  No markup here.
+
+== Description ==
+
+Pro Recent Post Widget plugin.You have choice to specific category recent post show.
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go Appearance => widgets section  see Pro Recent Posts
+
+== Frequently Asked Questions ==
+
+N/A
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.png.
+
+
+== Changelog ==
+
+N/A
+
+== Upgrade Notice ==
+
+N/A
